c7790062ec36c57eb0f1aa027d609cecc0791c09
Michael <michael.nilga@gmail.com>
2025-10-20 18:13:24 +0200

    Interfaces added

---
+++ CalculatorApp/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculatorApp.Interfaces
+{
+    public interface IAddition
+    {
+        double Add(double a, double b);
+    }
+
+    public interface ISubtraction
+    {
+        double Subtract(double a, double b);
+    }
+
+    public interface IMultiplication
+    {
+        double Multiply(double a, double b);
+    }
+
+    public interface IDivision
+    {
+        double Divide(double a, double b);
+    }
+}
